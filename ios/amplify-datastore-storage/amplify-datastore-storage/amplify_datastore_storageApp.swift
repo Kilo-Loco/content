4f41159d8ce81965284f912bb1284424e1c705d8
Kilo Loco <kiloloco@me.com>
2020-08-07 14:25:19 -0700

    iOS project

---
+++ ios/amplify-datastore-storage/amplify-datastore-storage/amplify_datastore_storageApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplify_datastore_storageApp.swift
+//  amplify-datastore-storage
+//
+//  Created by Kyle Lee on 8/7/20.
+//
+
+import SwiftUI
+
+@main
+struct amplify_datastore_storageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
