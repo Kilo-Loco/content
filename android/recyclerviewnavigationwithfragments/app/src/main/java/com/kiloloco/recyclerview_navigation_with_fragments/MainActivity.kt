73da35a529c09355a13b7ea0724ee206d3b33400
Kilo Loco <kiloloco@me.com>
2020-08-26 11:41:01 -0700

    saved

---
+++ android/recyclerviewnavigationwithfragments/app/src/main/java/com/kiloloco/recyclerview_navigation_with_fragments/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kiloloco.recyclerview_navigation_with_fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
