5edbd23e4df76492cd872d5f068883b4250c8f61
Kilo Loco <kiloloco@me.com>
2020-08-30 14:19:47 -0700

    Finished video project

---
+++ android/passing-data-with-safe-args/video/app/src/main/java/com/kiloloco/passing_data/User.kt
@@ -0,0 +1,10 @@
+package com.kiloloco.passing_data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class User(val id: Int, val name: String, val age: Int): Parcelable {
+    val description: String
+        get() = "$name is $age years old"
+}
---
+++ android/passing-data-with-safe-args/video/app/src/main/java/com/kiloloco/passing_data/UsersRecyclerViewAdapter.kt
@@ -0,0 +1,52 @@
+package com.kiloloco.passing_data
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.findNavController
+
+import com.kiloloco.passing_data.dummy.DummyContent.DummyItem
+
+/**
+ * [RecyclerView.Adapter] that can display a [DummyItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class UsersRecyclerViewAdapter(
+    private val users: List<User>
+) : RecyclerView.Adapter<UsersRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_users, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = users[position]
+        holder.idView.text = user.id.toString()
+        holder.contentView.text = user.name
+        holder.user = user
+    }
+
+    override fun getItemCount(): Int = users.size
+
+    inner class ViewHolder(view: View, var user: User? = null) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.item_number)
+        val contentView: TextView = view.findViewById(R.id.content)
+
+        init {
+            view.setOnClickListener {
+                user?.let {
+                    val directions = UsersFragmentDirections.actionUsersFragmentToUserDetailsFragment(it)
+                    view.findNavController().navigate(directions)
+                }
+            }
+        }
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+}
