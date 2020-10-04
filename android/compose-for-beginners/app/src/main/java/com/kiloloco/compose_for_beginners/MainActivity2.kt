f860983b3a217ce5cbe93f91a2bed36c7d553184
Kilo Loco <kiloloco@Kilos-MacBook-Pro.local>
2020-10-04 11:45:19 -0700

    start compose-for-beginners

---
+++ android/compose-for-beginners/app/src/main/java/com/kiloloco/compose_for_beginners/MainActivity2.kt
@@ -0,0 +1,38 @@
+package com.kiloloco.compose_for_beginners
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.kiloloco.compose_for_beginners.ui.ComposeforbeginnersTheme
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeforbeginnersTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeforbeginnersTheme {
+        Greeting("Android")
+    }
+}
