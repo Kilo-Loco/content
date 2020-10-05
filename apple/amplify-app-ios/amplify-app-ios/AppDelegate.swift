c44696ad9e63673ca26c1f05ba176cf00513a570
Kilo Loco <kiloloco@Kilos-MacBook-Pro.local>
2020-10-04 20:23:59 -0700

    amplify-app-ios start

---
+++ apple/amplify-app-ios/amplify-app-ios/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  amplify-app-ios
+//
+//  Created by Kilo Loco on 10/4/20.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+    
+}
---
+++ apple/amplify-app-ios/amplify-app-ios/amplify_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplify_app_iosApp.swift
+//  amplify-app-ios
+//
+//  Created by Kilo Loco on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct amplify_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
