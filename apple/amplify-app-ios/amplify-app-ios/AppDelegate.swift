fff3df1ec0a22c28c2817351b04a56924e219a44
Kilo Loco <kiloloco@Kilos-MacBook-Pro.local>
2020-10-04 22:37:13 -0700

    amplify-app-ios finish

--- apple/amplify-app-ios/amplify-app-ios/AppDelegate.swift
+++ apple/amplify-app-ios/amplify-app-ios/AppDelegate.swift
@@ -5,13 +5,47 @@
 //  Created by Kilo Loco on 10/4/20.
 //
 
+import Amplify
+import AmplifyPlugins
+import AWSPredictionsPlugin
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+        configureAmplify()
         return true
     }
     
+    
+    func configureAmplify() {
+        do {
+            // Auth
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            
+            // Analytics
+            try Amplify.add(plugin: AWSPinpointAnalyticsPlugin())
+            
+            // API
+            let models = AmplifyModels()
+            try Amplify.add(plugin: AWSAPIPlugin(modelRegistration: models))
+            
+            // DataStore
+            try Amplify.add(plugin: AWSDataStorePlugin(modelRegistration: models))
+            
+            // Predictions
+            try Amplify.add(plugin: AWSPredictionsPlugin())
+            
+            // Storage
+            try Amplify.add(plugin: AWSS3StoragePlugin())
+            
+            // Amplify configure
+            try Amplify.configure()
+            
+            print("Successfully configured Amplify!")
+            
+        } catch {
+            print("Could not initialize Amplify -", error)
+        }
+    }
 }
--- apple/amplify-app-ios/amplify-app-ios/amplify_app_iosApp.swift
+++ apple/amplify-app-ios/amplify-app-ios/amplify_app_iosApp.swift
@@ -5,13 +5,52 @@
 //  Created by Kilo Loco on 10/4/20.
 //
 
+import Amplify
+import AmplifyPlugins
+import AWSPredictionsPlugin
 import SwiftUI
 
 @main
 struct amplify_app_iosApp: App {
+    
+    init() {
+        configureAmplify()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    func configureAmplify() {
+        do {
+            // Auth
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            
+            // Analytics
+            try Amplify.add(plugin: AWSPinpointAnalyticsPlugin())
+            
+            // API
+            let models = AmplifyModels()
+            try Amplify.add(plugin: AWSAPIPlugin(modelRegistration: models))
+            
+            // DataStore
+            try Amplify.add(plugin: AWSDataStorePlugin(modelRegistration: models))
+            
+            // Predictions
+            try Amplify.add(plugin: AWSPredictionsPlugin())
+            
+            // Storage
+            try Amplify.add(plugin: AWSS3StoragePlugin())
+            
+            // Amplify configure
+            try Amplify.configure()
+            
+            print("Successfully configured Amplify!")
+            
+        } catch {
+            print("Could not initialize Amplify -", error)
         }
     }
 }
