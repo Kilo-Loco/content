e886a7acd177b2bf932f25a91ddb479b3d3900fb
Kilo Loco <kyle@kiloloco.com>
2020-10-09 18:11:26 -0700

    amplify-storage-getting-started-ios finished

--- apple/amplify-storage-getting-started/amplify-storage-getting-started/ContentView.swift
+++ apple/amplify-storage-getting-started/amplify-storage-getting-started/ContentView.swift
@@ -2,9 +2,9 @@
 //  ContentView.swift
 //  amplify-storage-getting-started
 //
-//  Created by Kilo Loco on 10/9/20.
+//  Created by Kilo Loco.
 //
-
+import Amplify
 import SwiftUI
 
 struct ContentView: View {
@@ -15,8 +15,37 @@ struct ContentView: View {
         if let fileStatus = self.fileStatus {
             Text(fileStatus)
         }
-        Button("Upload File", action: {}).padding()
+        Button("Upload File", action: uploadFile).padding()
     }
+    
+    func uploadFile() {
+        let fileKey = "testFile.txt"
+        let fileContents = "This is my dummy file"
+        let fileData = fileContents.data(using: .utf8)!
+        
+        Amplify.Storage.uploadData(
+            key: fileKey,
+            data: fileData
+        ) { result in
+            
+            switch result {
+            case .success(let key):
+                print("file with key \(key) uploaded")
+                
+                DispatchQueue.main.async {
+                    fileStatus = "File Uploaded"
+                }
+                
+            case .failure(let storageError):
+                print("Failed to upload file", storageError)
+                
+                DispatchQueue.main.async {
+                    fileStatus = "Failed to upload file"
+                }
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- apple/amplify-storage-getting-started/amplify-storage-getting-started/amplify_storage_getting_startedApp.swift
+++ apple/amplify-storage-getting-started/amplify-storage-getting-started/amplify_storage_getting_startedApp.swift
@@ -2,16 +2,39 @@
 //  amplify_storage_getting_startedApp.swift
 //  amplify-storage-getting-started
 //
-//  Created by Kilo Loco on 10/9/20.
+//  Created by Kilo Loco.
 //
 
+import Amplify
+import AmplifyPlugins
 import SwiftUI
 
 @main
 struct amplify_storage_getting_startedApp: App {
+    
+    init() {
+        configureAmplify()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+    func configureAmplify() {
+        do {
+            // Storage
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.add(plugin: AWSS3StoragePlugin())
+            
+            try Amplify.configure()
+            
+            print("Successfully configured Amplify 😁")
+            
+        } catch {
+            print("Could not configure Amplify", error)
+        }
+    }
+    
 }
