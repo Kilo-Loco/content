242b99adc8e12572c3c156a9b95c80018dcae21f
Kilo Loco <kyle@kiloloco.com>
2020-10-12 14:36:06 -0700

    amplify-api-realtime-chat start

---
+++ apple/amplify-api-realtime-chat/amplify-api-realtime-chat/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  amplify-api-realtime-chat
+//
+//  Created by Kilo Loco on 10/12/20.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
---
+++ apple/amplify-api-realtime-chat/amplify-api-realtime-chat/amplify_api_realtime_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplify_api_realtime_chatApp.swift
+//  amplify-api-realtime-chat
+//
+//  Created by Kilo Loco on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct amplify_api_realtime_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MessagesView()
+        }
+    }
+}
