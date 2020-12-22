a5eff44d8954c197af0864dc06ad7683cc18b7db
Kilo Loco <kyle@kiloloco.com>
2020-12-22 13:28:38 -0800

    swiftui-mvvm starter

---
+++ apple/swiftui-mvvm/swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Kilo Loco on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple/swiftui-mvvm/swiftui-mvvmTests/swiftui_mvvmTests.swift
@@ -0,0 +1,21 @@
+//
+//  swiftui_mvvmTests.swift
+//  swiftui-mvvmTests
+//
+//  Created by Kilo Loco on 12/22/20.
+//
+
+import XCTest
+@testable import swiftui_mvvm
+
+class swiftui_mvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
