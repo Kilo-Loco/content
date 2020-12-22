56a8339d29a826f0fd003029dfd18af39868da0a
Kilo Loco <kyle@kiloloco.com>
2020-12-22 13:35:42 -0800

    swiftui-mvvm start with UsersView

---
+++ apple/swiftui-mvvm/swiftui-mvvm/UsersView.swift
@@ -0,0 +1,20 @@
+//
+//  UsersView.swift
+//  swiftui-mvvm
+//
+//  Created by Kilo Loco on 12/22/20.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UsersView_Previews: PreviewProvider {
+    static var previews: some View {
+        UsersView()
+    }
+}
