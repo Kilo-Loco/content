2cec01b2fd25f70af77efa843f4cd765baff82ce
Kilo Loco <kyle@kiloloco.com>
2021-01-25 12:57:00 -0800

    access-photo-gallery-android finished

--- android/access-photo-gallery/app/src/main/java/com/kiloloco/access_photo_gallery/MainActivity.kt
+++ android/access-photo-gallery/app/src/main/java/com/kiloloco/access_photo_gallery/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kiloloco.access_photo_gallery
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts.GetContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Box
@@ -11,14 +13,19 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kiloloco.access_photo_gallery.ui.theme.AccessphotogalleryTheme
+import dev.chrisbanes.accompanist.glide.GlideImage
 
 class MainActivity : AppCompatActivity() {
+
+    private var imageUriState = mutableStateOf<Uri?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,13 +33,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val selectImageLauncher = registerForActivityResult(GetContent()) { uri ->
+        imageUriState.value = uri
+    }
+
     @Composable
     fun photoSelector() {
         Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
+                if (imageUriState.value != null) {
+                    GlideImage(data = imageUriState.value!!)
+                }
+
                 Button(
-                        onClick = { TODO() },
+                        onClick = { selectImageLauncher.launch("image/*") },
                         modifier = Modifier.padding(vertical = 8.dp)
                 ) {
                     androidx.compose.material.Text("Open Gallery")
