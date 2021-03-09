63c41c6bb370786effa880afca0f7d8faecb02ab
Kilo Loco <kyle@kiloloco.com>
2021-03-08 20:04:35 -0800

    siwa-swiftui finished

--- apple/siwa-swiftui/siwa-swiftui/ContentView.swift
+++ apple/siwa-swiftui/siwa-swiftui/ContentView.swift
@@ -5,17 +5,87 @@
 //  Created by Kilo Loco on 3/8/21.
 //
 
+import AuthenticationServices
 import SwiftUI
 
+struct AppleUser: Codable {
+    let userId: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    
+    init?(credentials: ASAuthorizationAppleIDCredential) {
+        guard
+            let firstName = credentials.fullName?.givenName,
+            let lastName = credentials.fullName?.familyName,
+            let email = credentials.email
+        else { return nil }
+        
+        self.userId = credentials.user
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+    }
+}
+
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SignInWithAppleButton(
+            .signIn,
+            onRequest: configure,
+            onCompletion: handle
+        )
+        .signInWithAppleButtonStyle(
+            colorScheme == .dark ? .white : .black
+        )
+        .frame(height: 45)
+        .padding()
+    }
+    
+    func configure(_ request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.fullName, .email]
+//        request.nonce = ""
+    }
+    
+    func handle(_ authResult: Result<ASAuthorization, Error>) {
+        switch authResult {
+        case .success(let auth):
+            print(auth)
+            switch auth.credential {
+            case let appleIdCredentials as ASAuthorizationAppleIDCredential:
+                if let appleUser = AppleUser(credentials: appleIdCredentials),
+                   let appleUserData = try? JSONEncoder().encode(appleUser) {
+                    UserDefaults.standard.setValue(appleUserData, forKey: appleUser.userId)
+                    
+                    print("saved apple user", appleUser)
+                } else {
+                    print("missing some fields", appleIdCredentials.email, appleIdCredentials.fullName, appleIdCredentials.user)
+                    
+                    guard
+                        let appleUserData = UserDefaults.standard.data(forKey: appleIdCredentials.user),
+                        let appleUser = try? JSONDecoder().decode(AppleUser.self, from: appleUserData)
+                    else { return }
+                    
+                    print(appleUser)
+                }
+                
+            default:
+                print(auth.credential)
+            }
+            
+        case .failure(let error):
+            print(error)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
