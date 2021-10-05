5c089743a9d6157b17a7a46278a4382863bfa924
Kilo Loco <kyle@kiloloco.com>
2021-10-05 05:25:20 -0700

    add-package-product-xcode initial

---
+++ apple/add-package-product-xcode/Add Package Product/Add_Package_ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  Add_Package_ProductApp.swift
+//  Add Package Product
+//
+//  Created by Kilo Loco on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Add_Package_ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple/add-package-product-xcode/Add Package Product/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Add Package Product
+//
+//  Created by Kilo Loco on 10/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
