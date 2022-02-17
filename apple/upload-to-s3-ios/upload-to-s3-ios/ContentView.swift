210fbc2c9e49349dc1b69dcec9baa4beedf9310c
Kilo Loco <kyle@kiloloco.com>
2022-02-17 13:34:11 -0800

    Initial upload-to-s3-ios

---
+++ apple/upload-to-s3-ios/upload-to-s3-ios/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  upload-to-s3-ios
+//
+//  Created by Kilo Loco on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Like and subscribe")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple/upload-to-s3-ios/upload-to-s3-ios/upload_to_s3_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  upload_to_s3_iosApp.swift
+//  upload-to-s3-ios
+//
+//  Created by Kilo Loco on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct upload_to_s3_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
