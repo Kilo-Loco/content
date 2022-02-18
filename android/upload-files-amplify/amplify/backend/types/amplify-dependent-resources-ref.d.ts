ad9e991ed1c42a13c6b821e59e81c44a354f60b5
Kilo Loco <kyle@kiloloco.com>
2022-02-18 11:04:11 -0800

    finish upload-files-amplify-android

---
+++ android/upload-files-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "uploadfilesamplify34a04271": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s37e2d2b41": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
