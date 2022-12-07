c7621c0d208b179a6a50620663733e517027f6fe
Kilo Loco <kyle@kiloloco.com>
2022-12-06 20:16:45 -0800

    latest

---
+++ apple/amplify-datastore-geo/amplify-datastore-geo/amplify_datastore_geoApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplify_datastore_geoApp.swift
+//  amplify-datastore-geo
+//
+//  Created by Kilo Loco on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct amplify_datastore_geoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
