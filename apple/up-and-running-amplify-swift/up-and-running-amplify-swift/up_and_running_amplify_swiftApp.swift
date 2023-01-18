0ba9400af4bfc838204764954a1642d49695b204
Kilo Loco <kyle@kiloloco.com>
2023-01-17 19:18:08 -0800

    up-and-running-amplify-swift final

--- apple/up-and-running-amplify-swift/up-and-running-amplify-swift/up_and_running_amplify_swiftApp.swift
+++ apple/up-and-running-amplify-swift/up-and-running-amplify-swift/up_and_running_amplify_swiftApp.swift
@@ -5,13 +5,28 @@
 //  Created by Kilo Loco on 1/16/23.
 //
 
+import Amplify
 import SwiftUI
 
 @main
 struct up_and_running_amplify_swiftApp: App {
+    
+    init() {
+        configureAmplify()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    private func configureAmplify() {
+        do {
+            try Amplify.configure()
+            print("Successfully configured Amplify")
+        } catch {
+            print("Could not configure Amplify", error)
         }
     }
 }
